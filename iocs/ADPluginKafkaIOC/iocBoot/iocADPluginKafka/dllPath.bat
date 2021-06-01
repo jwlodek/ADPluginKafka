67d3d4e24712b6bf5cf2beb739dddbe0f9ce9c07
jwlodek <jwlodek.dev@gmail.com>
2021-06-01 12:15:27 -0400

    Modifications to example IOC to have it build on newer system

---
+++ iocs/ADPluginKafkaIOC/iocBoot/iocADPluginKafka/dllPath.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+SET "PATH=/epics/src/support/areaDetector/ADPluginKafka/iocs/ADPluginKafkaIOC/bin/linux-x86_64;/epics/src/support/bin/linux-x86_64;/epics/src/support/asyn/bin/linux-x86_64;/epics/src/support/areaDetector/ADSupport/bin/linux-x86_64;/epics/src/support/autosave/bin/linux-x86_64;/epics/src/support/busy/bin/linux-x86_64;/epics/src/support/seq/bin/linux-x86_64;/epics/src/support/iocStats/bin/linux-x86_64;/epics/src/base/bin/linux-x86_64;%PATH%"
--- iocs/ADPluginKafkaIOC/iocBoot/iocADPluginKafka/st.cmd
+++ iocs/ADPluginKafkaIOC/iocBoot/iocADPluginKafka/st.cmd
@@ -4,24 +4,24 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/ADPluginKafkaApp.dbd")
 ADPluginKafkaApp_registerRecordDeviceDriver(pdbbase)
 
-epicsEnvSet("PREFIX", "$(PREFIX=DMSC)")
-epicsEnvSet("SIMDET_PORT", "$(PREFIX)SIMDET")
-epicsEnvSet("K_PORT", "$(PREFIX)K")
+epicsEnvSet("PREFIX", "13SIM1:")
+epicsEnvSet("SIMDET_PORT", "SIM1")
+epicsEnvSet("K_PORT", "KAFKA1")
 epicsEnvSet("XSIZE", "$(XSIZE=200)")
 epicsEnvSet("YSIZE", "$(YSIZE=200)")
 epicsEnvSet("QSIZE", "20")
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 simDetectorConfig("$(SIMDET_PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
-dbLoadRecords("$(SIMDET)/db/simDetector.template", "P=$(PREFIX):, R=SIM:, PORT=$(SIMDET_PORT), ADDR=0, TIMEOUT=1")
+dbLoadRecords("$(SIMDET)/db/simDetector.template", "P=$(PREFIX), R=cam1:, PORT=$(SIMDET_PORT), ADDR=0, TIMEOUT=1")
 
-KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(SIMDET_PORT)", 0, -1, "10.4.0.216:9092", "sim_data_topic", "some_source")
-dbLoadRecords("$(ADPLUGINKAFKA)/db/ADPluginKafka.template", "P=$(PREFIX),R=:KFK:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(SIMDET_PORT)")
+KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(SIMDET_PORT)", 0, -1, "localhost:9092", "sim_data_topic", "some_source")
+dbLoadRecords("$(ADPLUGINKAFKA)/db/ADPluginKafka.template", "P=$(PREFIX),R=KFK:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(SIMDET_PORT)")
 
 iocInit()
 
-dbpf $(PREFIX):KFK:EnableCallbacks Enable
-dbpf $(PREFIX):SIM:AcquirePeriod 2
-dbpf $(PREFIX):SIM:Acquire 1
+dbpf $(PREFIX)KFK:EnableCallbacks Enable
+dbpf $(PREFIX)cam1:AcquirePeriod 2
+dbpf $(PREFIX)cam1:Acquire 1
 
 #Always end with a new line
